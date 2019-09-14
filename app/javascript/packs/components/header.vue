9c4c12382597fc223217342ff65c641fff004188
tokumei20xx <oshi0824@gmail.com>
2019-09-14 12:17:33 +0900

    todo app #6のrouterの設定まで

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <ul id="dropdown" class="dropdown-content">
-      <li><a href="#">Top</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
+      <li><router-link class="black-text" to="/">Top</router-link></li>
+      <li><router-link class="black-text" to="/about">About</router-link></li>
+      <li><router-link class="black-text" to="/contact">Contact</router-link></li>
     </ul>
-    <nav>
+    <nav class="grey lighten-4">
       <div class="nav-wrapper container">
-        <a href="/" class="brand-logo left">Todo Application</a>
+        <a href="/" class="brand-logo left black-text">Todo</a>
         <ul class="right hide-on-med-and-down">
-          <li><a href="#">Top</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><router-link class="black-text" to="/">Top</router-link></li>
+          <li><router-link class="black-text" to="/about">About</router-link></li>
+          <li><router-link class="black-text" to="/contact">Contact</router-link></li>
         </ul>
         <ul class="right hide-on-large-only">
           <li>
-            <a class="dropdown-button" href="#!" data-activates="dropdown">
+            <a class="dropdown-button black-text" href="#!" data-activates="dropdown">
               Menu<i class="material-icons right">arrow_drop_down</i>
             </a>
           </li>
---
+++ app/javascript/packs/components/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Index</p>
+  </div>
+</template>
+app/javascript/packs/components/about.vue
+<template>
+  <div>
+    <!-- 内容はお好みで -->
+    <p>This is a sample of TODO application with Vue.js and Ruby on Rails.</p>
+    <p>Sample code is <a href="https://github.com/naoki85/todo_app_with_vue_and_rails" target="_blank">here.</a></p>
+  </div>
+</template>
+app/javascript/packs/components/contact.vue
+<template>
+  <div>
+    <!-- 内容はお好みで -->
+    <p>If you want to contact me, you send mail to below address.</p>
+    <p>test@example.com</p>
+  </div>
+</template>

9647711f75ef29830cb2e754c9a8a946415a8d52
tokumei20xx <oshi0824@gmail.com>
2019-09-15 09:40:35 +0900

    materialize 修正

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <ul id="dropdown" class="dropdown-content">
-      <li><router-link class="black-text" to="/">Top</router-link></li>
-      <li><router-link class="black-text" to="/about">About</router-link></li>
-      <li><router-link class="black-text" to="/contact">Contact</router-link></li>
+      <li><router-link to="/">Top</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
     </ul>
-    <nav class="grey lighten-4">
+    <nav>
       <div class="nav-wrapper container">
-        <a href="/" class="brand-logo left black-text">Todo</a>
+        <a href="/" class="brand-logo left">Todo</a>
         <ul class="right hide-on-med-and-down">
-          <li><router-link class="black-text" to="/">Top</router-link></li>
-          <li><router-link class="black-text" to="/about">About</router-link></li>
-          <li><router-link class="black-text" to="/contact">Contact</router-link></li>
+          <li><router-link to="/">Top</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+          <li><router-link to="/contact">Contact</router-link></li>
         </ul>
         <ul class="right hide-on-large-only">
           <li>
-            <a class="dropdown-button black-text" href="#!" data-activates="dropdown">
+            <a class="dropdown-button" href="#!" data-target="dropdown">
               Menu<i class="material-icons right">arrow_drop_down</i>
             </a>
           </li>
--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <p>Index</p>
-  </div>
-</template>
-app/javascript/packs/components/about.vue
-<template>
-  <div>
-    <!-- 内容はお好みで -->
-    <p>This is a sample of TODO application with Vue.js and Ruby on Rails.</p>
-    <p>Sample code is <a href="https://github.com/naoki85/todo_app_with_vue_and_rails" target="_blank">here.</a></p>
-  </div>
-</template>
-app/javascript/packs/components/contact.vue
-<template>
-  <div>
-    <!-- 内容はお好みで -->
     <p>If you want to contact me, you send mail to below address.</p>
     <p>test@example.com</p>
+
+    <!-- Modal Trigger -->
+    <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+
+    <!-- Modal Structure -->
+    <div id="modal1" class="modal">
+      <div class="modal-content">
+        <h4>Modal Header</h4>
+        <p>A bunch of text</p>
+      </div>
+      <div class="modal-footer">
+        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+      </div>
+    </div>
+ 
   </div>
 </template>
